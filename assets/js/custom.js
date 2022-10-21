5e069504d8965020c694ae675aa15d6a4bce0b3e
vaishali.jadhav@sundaymobility.com <vaishali.jadhav@sundaymobility.com>
2022-10-21 12:09:24 +0530

    json file added

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+fetch("./assets/data/data.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data.gallery);
+  })
+  .catch(function (err) {
+    console.log("error: " + err);
+  });
+function appendData(data) {
+  for (var i = 0; i < data.length; i++) {
+    const element =
+      '<div class="col-6 h-50 gallery-img-container"><img class="img-fluid object-fit-cover w-100 h-100" alt="image-' +
+      data[i].id +
+      '" src="' +
+      data[i].img +
+      '"></div>';
+    document.getElementById("galleryData").innerHTML += element;
+  }
+}
